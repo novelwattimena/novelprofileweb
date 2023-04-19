c614da70fccdd89b04e0e1d2ce6989a05ce7f36e
novelwattimena <novelthadeus@gmail.com>
2023-04-19 08:34:23 +0700

    commit april

--- script.js
+++ script.js
@@ -1,28 +1,62 @@
-// navbar
+// navbar kedua
 const nav = document.getElementById('home');
+// link
+const myElement = document.getElementById('my-element');
+// kode kedua
+const webnovel = document.querySelector('#welcome-message');
+
+
+
+let scrollposition = 0;
 
 window.addEventListener('scroll', function () {
     scrollposition = window.scrollY;
 
     if (scrollposition >= 60) {
         nav.classList.add('navbar');
-    } else if (this.scrollposition <= 60) {
-        nav.classList.remove('navbar')
+    } else if (scrollposition <= 60) {
+        nav.classList.remove('navbar');
     }
-})
-// end nvabar
+});
+// akhir navbar kedua
 
 
-// menu masuk
-const webnovel = document.querySelector('index.html');
 
+
+// link
+myElement.addEventListener('click', () => {
+  window.location.href = 'https://question.novelthadeus.repl.co';
+});
+
+
+
+
+// kode kedua
 let nama = localStorage.getItem('nama');
+const usernameRegex = /^[aA-zZ_]{5,}$/; // regex untuk username dengan minimal 5 karakter
 
-if (!nama) {
-    alert('Hello whats your name?')
-    nama = prompt('Enter your name: ');
+while (!nama || nama.trim() === '' || !usernameRegex.test(nama.trim())) {
+  const confirmed = confirm('Do you want to enter your name? 😁');
+
+  if (!confirmed) {
+    window.location.href = 'about:blank'; // keluar dari halaman
+    break;
+  }
+
+  nama = prompt('Hello, what is your username? ✋');
+
+  if (nama === null || nama.trim() === '' || !usernameRegex.test(nama.trim())) {
+    alert('You need to provide a valid username to use this site. Please make sure your username contains at least 5 characters and consists of only letters, and underscores. 🙏');
+  } else {
     localStorage.setItem('nama', nama);
-    webnovel.textContent = alert(`Hello ${nama} welcome to my web!`);
+    webnovel.innerHTML = alert(`Hello ${nama}, welcome to my web! 😍`);
+    window.location.href = 'index.html'; // masuk ke dalam halaman
+  }
 }
-webnovel.innerHTML = alert(`Hi ${nama} welcome back to my web!`);
-// end masuk
+
+if (nama && nama.trim() !== '') {
+  webnovel.innerHTML = alert(`Hi ${nama}, welcome back to my web! 😊`);
+}
+// akhir kode
+
+// video
